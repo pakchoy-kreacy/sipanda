8400e101b7e3bf1e6bf6f41cb2f2a05956fe2d78
pakchoy-kreacy <pakchoy-kreacy@users.noreply.github.com>
2026-07-15 14:50:55 +0700

    Fix: vercel.json tanpa functions block, api/config.js pake CommonJS

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.json({
     supabaseUrl: process.env.SUPABASE_URL || '',
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ''
   });
-}
+};
